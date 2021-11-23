39e5a78ab90228798db49ae1288c2d4ca6f17c3a
vietanhtran2710 <vietanhtran.uet@gmail.com>
2021-11-23 15:40:44 +0700

    Merge

--- subscriber-core/main.cpp
+++ subscriber-core/main.cpp
@@ -1,8 +1,66 @@
 #include <QCoreApplication>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <iostream>
+#include <jsoncpp/json/json.h>
+
+#define PORT 8080
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
+    int sock;
+    struct sockaddr_in serv_addr;
+    char buffer[1024] = {0};
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        printf("\n Socket creation error \n");
+        return 0;
+    }
 
-    return a.exec();
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)
+    {
+        std::cout << "Invalid address or address not supported" << std::endl;
+        return 0;
+    }
+
+    if (::connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    {
+        std::cout << "Connection failed!" << std::endl;
+        return 0;
+    }
+    Json::Value obj;
+    obj["command"] = "CONNECT";
+    Json::StyledWriter styledWriter; std::string jsonString = styledWriter.write(obj);
+    char json[1024] = {0};
+    strcpy(json, jsonString.c_str());
+    send(sock, json, sizeof(json), 0);
+    memset(buffer, 1024, 0);
+    recv(sock, buffer, sizeof(buffer), 0);
+    if (strncmp(buffer, "{\n", 2)==0)
+    {
+        Json::Value data; Json::Reader read;
+        std::string json(buffer);
+        read.parse(json, data);
+        if (data["status"] == "CONNECTACK") {
+            std::cout << "Connected" << std::endl;
+            obj["command"] = "SUBSCRIBE"; obj["topic"] = "/locationA/sensorA";
+            jsonString = styledWriter.write(obj);
+            char sendBuffer[1024] = {0};
+            strcpy(sendBuffer, jsonString.c_str());
+            send(sock, sendBuffer, sizeof(sendBuffer), 0);
+            while(true) {
+                memset(buffer, 1024, 0);
+                recv(sock, buffer, sizeof(buffer), 0);
+                if (strncmp(buffer, "{\n", 2)==0)
+                {
+                    read.parse(json, data);
+                    std::cout << buffer << std::endl;
+                }
+            }
+        }
+    }
+    else std::cout << "Not JSON\n";
 }
